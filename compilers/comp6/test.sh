f8b100279c90f7f863cc76caa32d2d33ff86a628
lukdz <kul1994@o2.pl>
2017-07-02 07:49:04 +0200

    test script update

--- compilers/comp6/test.sh
+++ compilers/comp6/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash   
 
-COMP="$(ghc Comp6)"
+#wybrana wersja zadania (5 lub 6)
+Version="6"
 
-FILES5="./JST/array1
+#lista plików z kodem źródłowym z rozrzerzeniem .pp5
+Files5="./JST/array1
 	./JST/arraySum1
 	./JST/arraySum2 
 	./JST/arraySum3 
@@ -23,40 +25,90 @@ FILES5="./JST/array1
 	./JST/simple
 	./JST/sum1
 	./JST/sum2
-	./JST/sum3"
+	./JST/sum3
+	./6PST/max
+	./6PST/roots0
+	./6PST/roots1
+	./6PST/roots2
+	./6PST/sum
+	./6PST/weird
+	./6ADT/fgh
+	./6ADT/length
+	./6DDT/ack
+	./6DDT/fibM
+	./6DDT/toBin"
 
-FILES6="./6LDT/fn1
+#lista plików z kodem źródłowym z rozrzerzeniem .pp6
+Files6="./6LDT/fn1
 	./6LDT/fn2
 	./6LDT/fn3
 	./6LDT/fn4
 	./6LDT/fn5
 	./6LDT/fn6
 	./6LDT/fn7
-	./6LDT/fn8"
+	./6LDT/fn8
+	./6AST/test_plik19
+	./6AST/test_plik20
+	./6AST/test_plik21
+	./6LDSKOS/e2
+	./6LDSKOS/l1
+	./6LDSKOS/l2_1
+	./6LDSKOS/l2_2
+	./6LDSKOS/l3
+	./6LDSKOS/l4_1
+	./6LDSKOS/l4_2
+	./6LDSKOS/l5
+	./6LDSKOS/l6
+	./6LDSKOS/l8
+	./6LDSKOS/l9
+	./6LDSKOS/t1_1
+	./6LDSKOS/t1_2
+	./6LDSKOS/t2
+	./6LDSKOS/t7"
 
+#kompilacja kompiltora
+COMP="$(ghc Comp$Version)"
+
+#wywołanie testów z rozszerzeniem .pp5
 echo "Prac5"
-for f in $FILES5
-do
-        COMP="$(./Comp6  $f.pp5)"
-	RESULT="$(./emu6809  $f.b09)"
-	LENGTH=${#f}
-	if [ $LENGTH -gt 15 ]
-	then echo -e "$f\t${RESULT}"
-	else echo -e "$f\t\t${RESULT}"
+for f in $Files5
+do	
+        COMP="$(./Comp$Version  $f.pp5)"
+	EMULA="$(./emu6809  $f.b09)"
+	INTER="$(./Prac$Version  $f.pp5)"
+	if [ ${#f} -gt 15 ]
+	then PATH="$f"
+	else PATH="$f\t"
+	fi
+	if [ "$EMULA" == "${INTER:6}" ]
+	then echo -e "$PATH\tOK"
+	else 	if [  "$INTER"  == "RuntimeError" ] && [  "$EMULA"  == "Division by zero" ];
+		then echo -e "$PATH\tOK"
+		else echo -e "$PATH\tFAIL\temu: $EMULA\tPrac: ${INTER}"
+		fi
 	fi
 done
 echo ""
 
+#wywołanie testów z rozszerzeniem .pp6
 echo "Prac6"
-for f in $FILES6
+for f in $Files6
 do
-        COMP="$(./Comp6  $f.pp6)"
-	RESULT="$(./emu6809  $f.b09)"
-	if [ $LENGTH -gt 15 ]
-	then echo -e "$f\t${RESULT}"
-	else echo -e "$f\t\t${RESULT}"
+        COMP="$(./Comp$Version  $f.pp6)"
+	EMULA="$(./emu6809  $f.b09)"
+	INTER="$(./Prac$Version  $f.pp6)"
+	if [ ${#f} -gt 15 ]
+	then PATH="$f"
+	else PATH="$f\t"
+	fi
+	if [ "$EMULA" == "${INTER:6}" ]
+	then echo -e "$PATH\tOK"
+	else 	if [  "$INTER"  == "RuntimeError" ] && [  "$EMULA"  == "Division by zero" ];
+		then echo -e "$PATH\tOK"
+		else echo -e "$PATH\tFAIL\temu: $EMULA\tPrac: ${INTER}"
+		fi
 	fi
 done
 echo ""
 
-echo "END"
+echo "test completed"
